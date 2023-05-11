2be8847f9cb0bf97baa1f84f727cb0b41c280e39
Bruno Futema <bruno.futema@outlook.com>
2023-05-11 01:00:55 -0300

    feat!: :sparkles: add breading change feature

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,6 +7,7 @@ export interface ButtonProps {
 export const Button: React.FC<ButtonProps> = ({ isLoading }) => {
   console.log('Add feature');
   console.log('Add more one feature');
+  console.log('Add breaking change feature');
   return (
     <div data-testid="button-container">
       {isLoading ? <p>Carregando</p> : <p>Button</p>}
